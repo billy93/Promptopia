f6cc18c2728825d67842748918fd940503d15b39
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-07-07 19:29:31 +0700

    fix: add cache no store to fetch

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,7 +7,9 @@ export const GET = async (request) => {
 
         const prompts = await Prompt.find({}).populate('creator')
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), { status: 200, headers: {
+            'Cache-Control': 'no-store', // Disable caching
+        }})
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
