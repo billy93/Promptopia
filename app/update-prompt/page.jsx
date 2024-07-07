1370465279dddd9d5b10cd346c4bc9af71d505ae
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-07-07 19:06:41 +0700

    fix: add cache no store to fetch

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -15,7 +15,7 @@ const UpdatePromptContent = () => {
 
   useEffect(() => {
     const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
+      const response = await fetch(`/api/prompt/${promptId}`, { cache: 'no-store' });
       const data = await response.json();
 
       setPost({
